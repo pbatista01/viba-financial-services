bfb1f0c839e11778b5d7727a5186bd75347b650b
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2025-09-28 14:09:14 -0500

    Modifying responsive design companies section

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -2,9 +2,9 @@ import companyLogos from '../data/companyLogos';
 
 function Companies() {
     return (
-        <section className="p-6 pt-16">
-            <h2 className="font-heading text-[1.4rem]">Comparamos más de 37 empresas con calificación A</h2>
-            <div className="grid grid-cols-2 gap-6 mt-10">
+        <section className="p-6 pt-20 lg:p-12 lg:pt-24 xl:p-20">
+            <h2 className="font-heading text-[1.4rem] lg:text-[1.5rem] xl:text-[1.6rem] 3xl:text-[2rem] text-dark text-center">Comparamos más de 37 empresas con calificación A</h2>
+            <div className="grid grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 items-center gap-6 mt-10">
                 {companyLogos.map((logo, index) => (
                     <img
                         key={index}
