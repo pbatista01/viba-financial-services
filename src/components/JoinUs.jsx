f10799bb41867f4b3b9990e89bf275ee685a43d9
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2025-10-04 20:02:54 -0500

    Changing font family for paragraphs on join us section

--- src/components/JoinUs.jsx
+++ src/components/JoinUs.jsx
@@ -5,10 +5,10 @@ function JoinUs(){
         <section className="p-6 pt-24 pb-24 xl:px-24 bg-sectionBg">
             <h2 className="font-heading text-[1.4rem] xl:text-[1.6rem] 3xl:text-[2rem] text-center text-primary">Se parte de nuestro equipo</h2>
            <div className="flex flex-col lg:flex-row lg:gap-6 2xl:gap-10 3xl:gap-16 lg:px-10 lg:mt-6 2xl:mt-16 xl:px-0  lg:items-center">
-            <div className="lg:w-full xl:w-[90%] 2xl:w-[75%] flex flex-col gap-2">
-                <h3 className="font-body text-dark text-[1.25rem] lg:text-[1.18rem] xl:text-[1.25rem] 2xl:text-[1.4rem] 3xl:text-[1.7rem] mt-10">Construye tu futuro ayudando a otros a proteger el suyo.</h3>
-                <p className="text-[1.1rem] lg:text-[0.9rem] xl:text-[1.1rem] 3xl:text-[1.3rem] mt-4">En ViBa Financial Services buscamos personas comprometidas, con vision y pasion por transformar vidas a traves de la educacion financiera.</p>
-                <span className="font-bold text-[1.1rem] lg:text-[0.9rem] xl:text-[1.1rem] 3xl:text-[1.3rem] text-primary">¿Te identificas? </span>
+            <div className="lg:w-full xl:w-[90%] 2xl:w-[75%] flex flex-col gap-2 lg:gap-4 2xl:gap-6">
+                <h3 className="font-body text-dark text-[1.25rem] lg:text-[1.2rem] xl:text-[1.3rem] 2xl:text-[1.5rem] 3xl:text-[1.7rem] mt-10">Construye tu futuro ayudando a otros a proteger el suyo.</h3>
+                <p className="font-body text-[1.1rem] lg:text-base xl:text-[1.2rem] 3xl:text-[1.4rem] mt-4 tracking-wide 3xl:leading-[30px]">En ViBa Financial Services buscamos personas comprometidas, con vision y pasion por transformar vidas a traves de la educacion financiera.</p>
+                <span className="font-bold font-body text-[1.1rem] lg:text-base xl:text-[1.2rem] 3xl:text-[1.4rem] text-primary">¿Te identificas? </span>
             </div>
             
             <div className="flex flex-col gap-3 py-4 lg:py-2 2xl:py-3 2xl:gap-6 px-2 w-[200px] lg:w-[400px] 2xl:w-[500px] border-2 border-primary text-dark rounded-lg mt-10 mx-auto">
